cb0b6d1b871d5e3906352bdef94dda834ad42f9b
Roman Tolmachev <tolmachevroman@gmail.com>
2024-01-06 09:30:12 -0300

    initial project setup

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Roman Tolmachev on 6.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
