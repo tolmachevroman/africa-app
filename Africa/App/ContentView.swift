3fbb447dcf45c322a09e75c4cf14ebc6cf8327e8
Roman Tolmachev <tolmachevroman@gmail.com>
2024-01-21 18:28:00 -0300

    add AnimalListItemView

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -9,11 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        let animals: [Animal] = Bundle.main.decode("animals.json")
+        
         NavigationView {
             List {
                 CoverImageView()
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                
+                ForEach(animals) { animal in
+                    AnimalListItemView(animal: animal)
+                }
             }
             .navigationBarTitle("Africa", displayMode: .large)
         }
