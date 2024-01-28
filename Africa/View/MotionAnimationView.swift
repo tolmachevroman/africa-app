24e5cf6e213022632c87c748dfbb7f45b0b1a7ba
Roman Tolmachev <tolmachevroman@gmail.com>
2024-01-28 08:44:56 -0300

    add MotionAnimationView

---
+++ Africa/View/MotionAnimationView.swift
@@ -0,0 +1,66 @@
+//
+//  MotionAnimationView.swift
+//  Africa
+//
+//  Created by Roman Tolmachev on 28.01.2024.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    
+    @State private var randomCircle = Int.random(in: 12...16)
+    @State private var isAnimating: Bool = false
+    
+    func randomCoordinate(max: CGFloat) -> CGFloat {
+        return CGFloat.random(in: 0...max)
+    }
+    
+    func randomSize() -> CGFloat {
+        return CGFloat(Int.random(in: 10...300))
+    }
+    
+    func randomScale() -> CGFloat {
+        return CGFloat(Double.random(in: 0.1...2.0))
+    }
+    
+    func randomSpeed() -> Double {
+        return Double.random(in: 0.025...1.0)
+    }
+    
+    func randomDelay() -> Double {
+        return Double.random(in: 0...2)
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                ForEach(0...randomCircle, id: \.self) { item in
+                    Circle()
+                        .foregroundColor(.gray)
+                        .opacity(0.15)
+                        .frame(width: randomSize(), alignment: .center)
+                        .scaleEffect(isAnimating ? randomScale() : 1)
+                        .position(
+                            x: randomCoordinate(max: geometry.size.width),
+                            y: randomCoordinate(max: geometry.size.height)
+                        )
+                        .onAppear {
+                            withAnimation(.interpolatingSpring(stiffness: 0.5, damping: 0.5)
+                                .repeatForever()
+                                .speed(randomSpeed())
+                                .delay(randomDelay())) {
+                                isAnimating = true
+                            }
+                        }
+                }
+                
+            }
+            .drawingGroup()
+        }
+    }
+}
+
+#Preview {
+    MotionAnimationView()
+}
